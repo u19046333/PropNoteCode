3535be347820842915ce72a577cdd84aaaf51ae6
u19046333 <u19046333@tuks.co.za>
2023-05-23 07:28:42 +0200

    AddingAllWork
    Other members git not working, so all pushed from one laptop. Contributions from all other members are included

--- Angular/src/app/broker/broker/broker.component.ts
+++ Angular/src/app/broker/broker/broker.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BrokerService } from 'src/app/services/broker.service';
+import { Broker } from 'src/app/shared/broker';
 
 @Component({
   selector: 'app-broker',
   templateUrl: './broker.component.html',
   styleUrls: ['./broker.component.scss']
 })
-export class BrokerComponent {
+export class BrokerComponent implements OnInit {
+  brokers: Broker[] = [];
+  dataSource = this.brokers;
+
+  constructor(private brokerService:BrokerService, private router:Router) {}
+
+  ngOnInit(): void {
+      this.GetBrokers();
+  }
+
+  GetBrokers() {
+    this.brokerService.GetAllBrokers().subscribe((result => {
+      let brokerList: any[] = result;
+      brokerList.forEach((element) => {
+        this.brokers.push(element);
+      })
+    }))
+  }
 
 }
---
+++ Angular/src/app/shared/broker.ts
@@ -0,0 +1,9 @@
+export interface Broker {
+  brokerID: Number;
+  name: String;
+  surname : String;
+  phoneNumber: String;
+  officeAddress: String;
+  licenseNumber: String;
+  commissionRate: String;
+}
